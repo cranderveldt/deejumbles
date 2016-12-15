f527744c285bf0957a105742840c7f5c8ec62077
cranderveldt <djnaphouse@gmail.com>
2016-12-15 12:59:15 -0500

    bingo card done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var app = angular.module('deejumbles', []);
 app.controller('Main',['$scope', function ($scope) {
-  $scope.bingo_tiles = [
+  $scope.cards = [[
     { state: "gray" }
     , { state: "gray" }
     , { state: "gray" }
@@ -16,21 +16,69 @@ app.controller('Main',['$scope', function ($scope) {
     , { state: "gray" }
     , { state: "gray" }
     , { state: "gray" }
-  ];
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+  ], [
+    { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+  ]];
   $scope.toggleTile = function(tile) {
     if (tile.state === "gray") {
       tile.state = "green";
-    }
-    if (tile.state === "green") {
+    } else if (tile.state === "green") {
       tile.state = "red";
-    }
-    if (tile.state === "red") {
+    } else if (tile.state === "red") {
       tile.state = "gray";
     }
   };
   $scope.bingoTileClasses = function(tile) {
     return "state-" + tile.state;
   };
+  $scope.randomSecureString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  };
+  $scope.randomTitle = function() {
+    return "Isaac Bingo - " + $scope.randomSecureString(5);
+  };
+  document.title = $scope.randomTitle();
 }]);
 // app.directive('tlRotate', function () {
 //   return {
