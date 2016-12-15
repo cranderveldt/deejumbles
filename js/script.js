629cef33f65ad2f7ef57d8f78ebe4f7bdb648fa6
cranderveldt <djnaphouse@gmail.com>
2016-12-15 12:38:42 -0500

    bingo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 var app = angular.module('deejumbles', []);
 app.controller('Main',['$scope', function ($scope) {
-  
+  $scope.bingo_tiles = [
+    { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+    , { state: "gray" }
+  ];
+  $scope.toggleTile = function(tile) {
+    if (tile.state === "gray") {
+      tile.state = "green";
+    }
+    if (tile.state === "green") {
+      tile.state = "red";
+    }
+    if (tile.state === "red") {
+      tile.state = "gray";
+    }
+  };
+  $scope.bingoTileClasses = function(tile) {
+    return "state-" + tile.state;
+  };
 }]);
 // app.directive('tlRotate', function () {
 //   return {
